36e886ca99c929157638692a75648ee192f11271
SujenShrestha <sujenpro@gmail.com>
2023-04-11 15:10:56 +0545

    NIST password guidelines

---
+++ nist_compliance.py
@@ -0,0 +1,13 @@
+def display_password_compliance():
+    nist_guidelines = [
+        "Passwords should be at least 8 characters in length.",
+        "Include a combination of upper and lower case letters, numbers, and special characters.",
+        "Avoid dictionary words, common phrases, and easily guessable information.",
+        "Avoid using easily guessable personal information, such as birthdays or names of family members.",
+        "Use a unique password for each account.",
+        "Do not write down or share passwords with others.",
+        "Enable multi-factor authentication (MFA) when available.",
+        "Do not use passwords that have been compromised in data breaches."
+    ]
+
+    return nist_guidelines
